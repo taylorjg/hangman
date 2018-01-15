359b7e4c3bbca02a8c4ff51620205a6cce3edfee
Jonathan Taylor <jont@cakesolutions.net>
2018-01-15 20:27:31 +0000

    Clean up ControlPanel

--- src/Components/ControlPanel.js
+++ src/Components/ControlPanel.js
@@ -2,31 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import * as C from '../constants';
 
-const ControlPanel = (props) => {
+const WON_MESSAGE = "You won!";
+const LOST_MESSAGE = "You lost!";
+
+const renderMessage = props =>
+    props.gameState === C.GAME_STATE_GAME_OVER
+        ? props.outcome === C.OUTCOME_WON
+            ? <p>{WON_MESSAGE}</p>
+            : <p>{LOST_MESSAGE}</p>
+        : "";
+
+const renderButton = props =>
+    props.gameState === C.GAME_STATE_GAME_OVER
+        ? <button onClick={props.onNewGame}>New Game</button>
+        : "";
+
+const ControlPanel = props => {
     return (
         <div>
-            {(() => {
-                switch (props.gameState) {
-                    case C.GAME_STATE_GAME_OVER:
-                        switch (props.outcome) {
-                            case C.OUTCOME_WON:
-                                return <p>GAME OVER | WON</p>;
-                            case C.OUTCOME_LOST:
-                                return <p>GAME OVER | LOST</p>;
-                            default:
-                                return <p>Unknown game state</p>;
-                        }
-                    case C.GAME_STATE_IN_PROGRESS:
-                        return <p>IN PROGRESS</p>;
-                    default:
-                        return <p>Unknown outcome</p>;
-                }
-            })()}
-            {
-                props.gameState === C.GAME_STATE_GAME_OVER
-                ? <button onClick={props.onNewGame}>New Game</button>
-                : ""
-            }
+            {renderMessage(props)}
+            {renderButton(props)}
         </div>
     );
 };
