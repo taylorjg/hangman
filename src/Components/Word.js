cce0a665c06279a0a57aa247679a7e39b93d6d3b
Jonathan Taylor <jont@cakesolutions.net>
2018-01-24 19:33:52 +0000

    Add web api error handling

--- src/Components/Word.js
+++ src/Components/Word.js
@@ -4,16 +4,32 @@ import * as C from '../constants';
 import './Word.css';
 
 const Word = ({ gameState, word, goodGuesses }) => {
+
+  const renderWord = () => {
+    switch (gameState) {
+
+      case C.GAME_STATE_IN_PROGRESS:
+        return (
+          <span className="Word">
+            {
+              Array.from(word).map(letter =>
+                goodGuesses.includes(letter) ? letter : '-')
+            }
+          </span>
+        )
+
+      case C.GAME_STATE_GAME_OVER:
+        return <span className="Word">{word}</span>
+
+      case C.GAME_STATE_CHOOSING_WORD:
+      default:
+        return <span className="Word-loading">(choosing a word...)</span>
+    }
+  };
+
   return (
     <p>
-      <span className='Word'>
-        {
-          gameState === C.GAME_STATE_IN_PROGRESS
-            ? Array.from(word).map(letter =>
-              goodGuesses.includes(letter) ? letter : '-')
-            : word || '?????'
-        }
-      </span>
+      {renderWord()}
     </p>
   );
 };
--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,12 @@
 import axios from 'axios';
+import { fallbackWord } from '../wordList';
 
 export const api = {
   chooseWord: () =>
-    axios.get(
-      '/api/chooseWord',
-      {
-        headers: {
-          'accept': 'application/json'
-        }
-      })
+    axios.get('/api/chooseWord')
       .then(response => response.data)
+      .catch(error => ({
+        word: fallbackWord(),
+        errorMessage: error.message
+      }))
 };
