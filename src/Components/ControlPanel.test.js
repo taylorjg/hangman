0641a60965376b89b492e829762160fa9516815b
Jonathan Taylor <jont@cakesolutions.net>
2018-01-28 12:10:31 +0000

    Add remaining Component unit tests

--- src/Components/ControlPanel.test.js
+++ src/Components/ControlPanel.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import sinon from 'sinon';
 import ControlPanel from './ControlPanel';
 import * as C from '../constants';
 
@@ -13,37 +12,41 @@ it('renders correctly when choosing a word', () => {
 });
 
 it('renders correctly when game is in progress', () => {
-  const wrapper = shallow(<ControlPanel
-    gameState={C.GAME_STATE_IN_PROGRESS}
-    outcome={C.OUTCOME_NONE}
-    onNewGame={() => { }} />);
+  const onNewGame = jest.fn();
+  const wrapper = shallow(
+    <ControlPanel
+      gameState={C.GAME_STATE_IN_PROGRESS}
+      outcome={C.OUTCOME_NONE}
+      onNewGame={onNewGame} />);
   expect(wrapper.children().length).toEqual(0);
 });
 
 it('renders correctly when the game is over (won)', () => {
-  const onNewGame = sinon.spy();
-  const wrapper = shallow(<ControlPanel
-    gameState={C.GAME_STATE_GAME_OVER}
-    outcome={C.OUTCOME_WON}
-    onNewGame={onNewGame} />);
+  const onNewGame = jest.fn();
+  const wrapper = shallow(
+    <ControlPanel
+      gameState={C.GAME_STATE_GAME_OVER}
+      outcome={C.OUTCOME_WON}
+      onNewGame={onNewGame} />);
   const message = <p>You won!</p>;
   expect(wrapper.contains(message)).toEqual(true);
   expect(wrapper.find('button').length).toEqual(1);
-  expect(wrapper.find('button').text()).toEqual("New Game");
+  expect(wrapper.find('button').text()).toEqual('New Game');
   wrapper.find('button').simulate('click');
-  expect(onNewGame.callCount).toEqual(1);
+  expect(onNewGame).toHaveBeenCalled();
 });
 
 it('renders correctly when the game is over (lost)', () => {
-  const onNewGame = sinon.spy();
-  const wrapper = shallow(<ControlPanel
-    gameState={C.GAME_STATE_GAME_OVER}
-    outcome={C.OUTCOME_LOST}
-    onNewGame={onNewGame} />);
+  const onNewGame = jest.fn();
+  const wrapper = shallow(
+    <ControlPanel
+      gameState={C.GAME_STATE_GAME_OVER}
+      outcome={C.OUTCOME_LOST}
+      onNewGame={onNewGame} />);
   const message = <p>You lost!</p>;
   expect(wrapper.contains(message)).toEqual(true);
   expect(wrapper.find('button').length).toEqual(1);
-  expect(wrapper.find('button').text()).toEqual("New Game");
+  expect(wrapper.find('button').text()).toEqual('New Game');
   wrapper.find('button').simulate('click');
-  expect(onNewGame.callCount).toEqual(1);
+  expect(onNewGame).toHaveBeenCalled();
 });
---
+++ src/Components/Gallows.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Gallows from './Gallows';
+import { MAX_BAD_GUESSES } from '../constants';
+
+const helper = badGuesses => {
+  const wrapper = mount(<Gallows badGuesses={badGuesses} />);
+  expect(wrapper.find('svg').length).toEqual(1);
+  const svgNode = wrapper.getDOMNode();
+  expect(svgNode.querySelectorAll('path').length).toEqual(badGuesses.length);
+};
+
+it('renders correctly for the given number of bad guesses', () => {
+  const LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const range = Array.from(Array(MAX_BAD_GUESSES + 1).keys());
+  range.forEach(n => helper(LETTERS.substr(0, n)));
+});
